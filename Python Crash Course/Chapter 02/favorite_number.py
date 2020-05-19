5e5936a5ce8978aed62bdd4d609be6248ca304d2
XL'anna Brooks <pandajunkay@yahoo.com>
2020-05-19 18:50:17 -0400

    Python Crash Course - Exercises 2.8 and 2.9

---
+++ Python Crash Course/Chapter 02/favorite_number.py
@@ -0,0 +1,5 @@
+# Exercise 2.9
+
+favorite_number = 3
+message = f"My favorite number is {favorite_number}."
+print(message)
---
+++ Python Crash Course/Chapter 02/number_eight.py
@@ -0,0 +1,6 @@
+# Exercise 2.8
+
+print(5 + 3)
+print(10 - 2)
+print(2 * 4)
+print(24 / 3)
