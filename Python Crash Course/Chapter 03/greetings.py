f8ae06c88211d7e962b8cf7c5b522a660b4d596e
XL'anna Brooks <pandajunkay@yahoo.com>
2020-05-20 21:07:55 -0400

    Python Crash Course - Exercises 3.1 and 3.2

---
+++ Python Crash Course/Chapter 03/greetings.py
@@ -0,0 +1,15 @@
+# Exercise 3.2
+
+names = ['jamie', 'hyatt', 'hewitt', 'skye']
+
+message = f"Hello {names[0].title()}, how is your day?"
+print(message)
+
+message = f"Hello {names[1].title()}, how is your day?"
+print(message)
+
+message = f"Hello {names[2].title()}, how is your day?"
+print(message)
+
+message = f"Hello {names[3].title()}, how is your day?"
+print(message)
---
+++ Python Crash Course/Chapter 03/names.py
@@ -0,0 +1,8 @@
+# Exercise 3.1
+
+names = ['jamie', 'hyatt', 'hewitt', 'skye']
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[-1])
