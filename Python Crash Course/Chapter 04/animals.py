a66424022ab83f82ee50761a40507b213a76aa3f
XL'anna Brooks <pandajunkay@yahoo.com>
2020-05-25 17:47:59 -0400

    Python Crash Course: Exercises 4.1 and 4.2

---
+++ Python Crash Course/Chapter 04/animals.py
@@ -0,0 +1,8 @@
+# Exercise 4.2
+
+animals = ['cat', 'dog', 'bearded dragon']
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print("\nAll of these animals would make a great pet!")
---
+++ Python Crash Course/Chapter 04/pizza.py
@@ -0,0 +1,7 @@
+# Exercise 4.1
+
+pizzas = ['cheese', 'pepperoni', 'supreme']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("\nI really love pizza!")
